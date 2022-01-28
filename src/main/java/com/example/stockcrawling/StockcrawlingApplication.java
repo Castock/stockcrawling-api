c878057d5c5debd5b0aa56e2aef8f28708784cb1
ckdwlsrh <ckdwls6504@gmail.com>
2022-01-28 15:49:17 +0900

    first commit

---
+++ src/main/java/com/example/stockcrawling/StockcrawlingApplication.java
@@ -0,0 +1,13 @@
+package com.example.stockcrawling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockcrawlingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockcrawlingApplication.class, args);
+	}
+
+}
